bccc8cb70cd1885c35f46e92117a4afd9fc28c75
Snehit14 <shindesnehit@gmail.com>
2021-12-27 13:19:17 +0530

    code

--- Dev fjp/Js intro/functions.js
+++ Dev fjp/Js intro/functions.js
@@ -9,7 +9,14 @@ add(5,4) //function invokation , 5 and 4 are arguments
 //with the use of return statement
 function add1(c,d){
     return(c+d)
-} //function declaration , a and b are parameters
+} //function declaration , c and d are parameters
 
 let sum = add1(6,10) //function invokation , 6 and 10 are arguments
 console.log(sum)
+
+//in JavaSript functions are known as first class Citizens
+let sayHi = function(s){
+    console.log(s)
+}//anonymous functions -- the function with no name
+
+sayHi(4)
