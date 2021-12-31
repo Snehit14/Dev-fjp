17be30ceae67cb8511030aff7f2465713087dd48
Snehit14 <shindesnehit@gmail.com>
2021-12-31 11:54:31 +0530

    DEV

---
+++ Dev fjp/Mini Project/CliResume.js
@@ -0,0 +1,57 @@
+var inquirer = require('inquirer');
+const cp = require('child_process');
+const { Console } = require('console');
+
+
+function displayDetails(){
+inquirer
+  .prompt([
+    {
+           type:'list',
+           name :'Selection',
+           choices:['About' , 'Skills' , 'Projects' , 'Academics']
+    }
+  ])
+  .then((choices) => {
+       if(choices.Selection =='About'){
+              console.log(`A passionate coder and web developer who loves to code and explore new technology.`)
+              displayNext()
+       }
+
+       else if(choices.Selection=='Skills'){
+              console.log('JavaScript , Node , React , Java , DSA problem solving')
+              displayNext()
+       }
+
+       else if(choices.Selection=='Projects'){
+            cp.execSync('start chrome https://github.com/Snehit14')
+            displayNext()
+       }
+
+       else if(choices.Selection=='Academics'){
+              console.log('Good in academics with 9.1 sgpa till 4th semester in Computer Engineering')
+              displayNext()
+       }
+  })
+ 
+}
+
+displayDetails()
+
+
+function displayNext(){
+       var inquirer = require('inquirer');
+inquirer
+  .prompt([
+         { type:'list',
+       name :'GoBack',
+       choices:['back']
+         }
+  ])
+  .then((choice) => {
+    if(choice.GoBack = 'back'){
+           displayDetails()
+    }
+  })
+ 
+}
